fbef8c03226447c80aa7607784655d703b71db23
Elena-R-P <75149529+Elena-R-P@users.noreply.github.com>
2021-04-27 13:59:13 -0500

    Update settings.py

--- great_number_game/settings.py
+++ great_number_game/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1$kzt6%p&f*lz2f89@khzm57o$d6$rn7pt5m)7jh3_%%k)^tp)'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
